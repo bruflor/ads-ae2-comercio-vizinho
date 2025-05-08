d29f31db15894e5f4f010111742dfb2d6c5d3a57
Wes Benvindo <wal2.designer@gmail.com>
2025-05-08 21:29:20 +0100

    wip: passing dynamic attrbutes and some interfaces to testing custom styles and props

--- ComercioVizinho/MVC/Components/CategoryCardViewComponent.cs
+++ ComercioVizinho/MVC/Components/CategoryCardViewComponent.cs
@@ -3,10 +3,18 @@ using MVC.Models;
 
 namespace MVC.Components;
 
-public class CategoryCardViewComponent: ViewComponent
+public class CategoryCardViewComponent : ViewComponent
 {
     public IViewComponentResult Invoke(Category categories)
     {
+        Console.WriteLine(categories);
         return View(categories);
     }
+}
+
+public interface IWidth
+{
+    void setWidth(string width);
+    String? getWidth ();
+    
 }
