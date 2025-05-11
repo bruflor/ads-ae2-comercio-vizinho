676f2faee092430f2dbcb1d4b4b09e0a11ab7037
Bruna <brn.flor@gmail.com>
2025-05-11 13:04:48 +0100

    Adding product page default

---
+++ ComercioVizinho/MVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+
+namespace MVC.Controllers;
+
+public class ProductController: Controller
+{
+    private readonly ComercioVizinhoDbContext _context;
+
+    public ProductController(ComercioVizinhoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        ViewData["Products"] = await _context.Products.Include(p => p.Producer).ToListAsync();
+        return View();
+    }
+}
