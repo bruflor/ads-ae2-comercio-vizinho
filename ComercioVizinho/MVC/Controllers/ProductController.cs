e4804647b53708f6d1b16e9dd3cb61a5c736fccf
Bruna <brn.flor@gmail.com>
2025-05-12 20:50:06 +0100

    Adding the categories data into the controler of the detail product. Some render option working into detail page

--- ComercioVizinho/MVC/Controllers/ProductController.cs
+++ ComercioVizinho/MVC/Controllers/ProductController.cs
@@ -18,9 +18,18 @@ public class ProductController: Controller
         ViewData["Products"] = await _context.Products.Include(p => p.Producer).ToListAsync();
         return View();
     }
+
     public async Task<IActionResult> Detail(Guid id)
     {
-        ViewData["Product"] = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
-        return View();
+        var product = await _context.Products
+            .Include(x => x.Categories) 
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product); 
     }
 }
